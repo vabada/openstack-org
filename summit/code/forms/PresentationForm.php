02fe7acb8cbbd5ad7e92e3497b7ff51465fb5076
jimmytipit <jimmy@tipit.net>
2015-12-18 15:53:47 -0600

    Fixes to CfS navigation

--- summit/code/forms/PresentationForm.php
+++ summit/code/forms/PresentationForm.php
@@ -32,7 +32,7 @@ final class PresentationForm extends BootstrapForm
                     ->setEmptyString('-- Select one --')
                     ->setSource(Presentation::create()->dbObject('Level')->enumValues())
                 ->end()
-            ->literal('AbstractHelp','<hr/><p>YouTube and other services limit the length of your presentation\'s description. Please provide a shorter, YouTube-friendly summary below.</p>')
+            ->literal('AbstractHelp','<hr/><p>YouTube and other services limit the length of your presentation\'s description. We will take the first 100 characters of your abstract to display in the YouTube description.</p>')
             ->tinyMCEEditor('ShortDescription','Abstract (1000 chars)')
                 ->configure()
                     ->setRows(20)
