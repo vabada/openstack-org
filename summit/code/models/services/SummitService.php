7418393b30c1dd9d09cecb609f7dc3f25d84312e
spalenque <santipalenque@gmail.com>
2016-04-28 14:45:08 -0300

    [spalenque] - #10988 - video report fix dup speakers and save bug
    Conflicts:
    	summit/code/models/services/SummitService.php

--- summit/code/models/services/SummitService.php
+++ summit/code/models/services/SummitService.php
@@ -710,9 +710,8 @@ final class SummitService implements ISummitService
 
                 foreach ($event->Materials()->filter('ClassName','PresentationVideo') as $video) {
                     $video->DisplayOnSite = intval($event_data['display_video']);
-                    $video->write;
+                    $video->write();
                 }
-                $event->write();
             }
         });
     }
