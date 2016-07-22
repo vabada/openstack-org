df071a7fc67ace2edd84c359af3a7c73f40ecce6
spalenque <santipalenque@gmail.com>
2016-07-22 12:43:06 -0300

    [spalenque] - #11445 - fix summit admin crud events: panels not showing speakers or moderators

--- summit/code/models/services/SummitService.php
+++ summit/code/models/services/SummitService.php
@@ -288,7 +288,7 @@ final class SummitService implements ISummitService
 
             $event->Speakers()->setByIDList($speaker_ids);
 
-            if($event->Type()->Type == 'Keynotes')
+            if($event->Type()->Type == 'Keynotes' || $event->Type()->Type == 'Panel')
             {
                 if(!isset($event_data['moderator']))
                     throw new EntityValidationException('moderator is required!');
@@ -343,7 +343,7 @@ final class SummitService implements ISummitService
      * @return bool
      */
     public static function IsPresentationEventType($type){
-        return ($type === 'Presentation' || $type === 'Keynotes');
+        return ($type === 'Presentation' || $type === 'Keynotes' || $type === 'Panel');
     }
 
     /**
