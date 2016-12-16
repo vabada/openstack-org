ab9ff026b2370d42811f595e36a69063c2b11e47
Sebastian Marcet <smarcet@gmail.com>
2016-12-16 18:58:15 -0300

    [smarcet] - #12178 * added new presentation event type LightingTalks * updated data model ( event types / presentation types) * updated business rules * updated cms admin UI/ summit admin and call for speakers * added migration to remove column LightningTalk from Presentation Table

--- summit/code/controllers/SummitAppAdminController.php
+++ summit/code/controllers/SummitAppAdminController.php
@@ -747,7 +747,7 @@ final class SummitAppAdminController extends Controller implements PermissionPro
      * @return bool
      */
     public function IsPresentationEventType($type){
-        return SummitService::IsPresentationEventType($type);
+        return PresentationType::IsPresentationEventType($type);
     }
 
     /**
@@ -755,7 +755,7 @@ final class SummitAppAdminController extends Controller implements PermissionPro
      * @return bool
      */
     public function IsSummitEventType($type){
-        return SummitService::IsSummitEventType($type);
+        return SummitEventType::IsSummitEventType($type);
     }
 
     public function Time(){
