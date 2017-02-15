d7b1fa5c09e4a56159cfb6cd8647c1cc76d62c30
spalenque <santipalenque@gmail.com>
2017-02-15 14:02:29 -0300

    [spalenque] * hotfix on summit voting app show abstract

--- summit-voting-app/code/PresentationVotingPage.php
+++ summit-voting-app/code/PresentationVotingPage.php
@@ -291,7 +291,7 @@ class PresentationVotingPage_API extends RequestHandler
                 'date' => $vote->obj('Created')->Format('F j, Y'),
                 'ago' => $vote->obj('Created')->Ago()
             ] : null,
-            'abstract' => $presentation->Description,
+            'abstract' => $presentation->Abstract,
             'attendees_expected_learnt' => $presentation->AttendeesExpectedLearnt
         ];
 
